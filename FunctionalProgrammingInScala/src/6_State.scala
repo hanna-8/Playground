68f258663f4b4532e96ab17310a02852d5900514
Ioana Barcan <ibarcan@endava.net>
2016-09-28 17:59:47 +0300

    Chapter 6 - v2

--- FunctionalProgrammingInScala/src/6_State.scala
+++ FunctionalProgrammingInScala/src/6_State.scala
@@ -1,7 +1,4 @@
-import scala.annotation.tailrec
-
 object Chapter6_State {
-
 
   object State {
     def unit[S, A](a: A): State[S, A] = State(s => (a, s))
@@ -39,23 +36,38 @@ object Chapter6_State {
   sealed trait Input
   case object Coin extends Input
   case object Knob extends Input
-//
-//  case class Machine(locked: Boolean, candies: Int, coins: Int) {
-//
-//    def insertCoin: Machine = (locked, candies, coins) match {
-//      case (true, cas, cos) if (cas > 0) => Machine(false, cas, cos + 1)
-//      case (_, _, _) => this
-//    }
-//
-//    def turnKnob: Machine = (locked, candies, coins) match {
-//      case (false, cas, cos) if (cos > 0) => Machine(true, cas - 1, cos)
-//      case (_, _, _) => this
-//    }
+
+  def insertCoin(m: Machine): Machine = (m.l, m.s, m.c) match {
+    case (true, s, c) if (s > 1) => Machine(false, s, c + 1)
+    case _ => m
+  }
+
+  def turnKnob(m: Machine): Machine = (m.l, m.s, m.c) match {
+    case (false, s, c) if (s > 1) => Machine(true, s - 1, c)
+    case _ => m
+  }
+
+  case class Machine(l: Boolean, s: Int, c: Int)
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+    inputs.foldLeft(
+      State.unit[Machine, (Int, Int)]((0 ,0))
+    )((_, i) => for {
+      m <- State.get
+      _ <- State.set(if (i == Coin) insertCoin(m) else turnKnob(m))
+      m2 <- State.get
+      //m1 <- State.modify(if (i == Coin) insertCoin else turnKnob)
+    } yield(m2.s, m2.c))
+  }
+
+//  def updateMachine(m: Machine, i: Input): Machine = (m, i) match {
+//    case (m, _) if (m.s < 1) => m
+//    case (m, Coin) if (m.l == true) => Machine(false, m.s, m.c + 1)
+//    case (m, Knob) if (m.l == false) => Machine(true, m.s - 1, m.c)
 //  }
-//
-//  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = for {
-//
-//  }
+
+
+
 //
 //    (m: Machine) => {
 //    def go(ins: List[Input], acc: State[Machine, (Int, Int)]): State[Machine, (Int, Int)] = {
@@ -72,9 +84,8 @@ object Chapter6_State {
 //  }
 
   def main(args: Array[String]) : Unit = {
-    //val m = Machine(true, 5, 10)
-    //val (coins, candies) = simulateMachine(List(Coin, Knob, Coin, Knob, Coin, Knob, Coin, Knob)).run(m)
-    //println("coins: " + coins + "; candies: " + candies)
-    //println(State.zipIndex(List('a', 'b', 'c')))
+    val m = Machine(true, 5, 10)
+    val (coins, candies) = simulateMachine(List(Coin, Knob, Coin, Knob, Coin, Knob, Coin, Knob)).run(m)._1
+    println("coins: " + coins + "; candies: " + candies)
   }
 }
