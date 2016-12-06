58b97b636f2819ae1768c11dac79612a771b489f
Ioana Barcan <ioana.barcan@endava.com>
2016-12-06 17:12:52 +0200

    FPiS ch9: Updated the Parser with flatMap. Exs. 9.6, 9.7 and 9.8.

--- FunctionalProgrammingInScala/src/9_parse.scala
+++ FunctionalProgrammingInScala/src/9_parse.scala
@@ -4,73 +4,100 @@ import scala.util.matching.Regex
 
 object ch9_parse {
 
-  trait Parsers [ParseError, Parser[+_]] { self =>
-
-    implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]):
-      ParserOps[String] = ParserOps(f(a))
-    //    implicit def asCharParser[A](a: A)(implicit f: A => Parser[Char]):
-    //      ParserOps[Char] = ParserOps(f(a))
-
-    implicit def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
-
-    // Recognizes and returns a single string
-    implicit def string(s: String): Parser[String] = ???
+  trait Parsers [ParseError, Parser[+_]] {
+    self =>
 
     implicit def operations[A](p: Parser[A]) = ParserOps[A](p)
 
-    implicit def regex(rx: Regex): Parser[String] = ???
+    //    implicit def asCharParser[A](a: A)(implicit f: A => Parser[Char]):
+    //      ParserOps[Char] = ParserOps(f(a))
+    implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]):
+    ParserOps[String] = ParserOps(f(a))
+
+    implicit def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
 
     def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B] = ???
-
-    // Choose between p1 and p2. First attempt: p1. If p1 fails, choose p2.
-    def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A] = ???
 
     // map2 succeed
     def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] = n match {
       case 0 => succeed(List.empty)
-      case i => p.map2(listOfN(i - 1, p))(_+:_)
+      case i => p.map2(listOfN(i - 1, p))(_ +: _)
     }
 
     // Recognize 0 or more repetitions of a-s.
     def many[A](p: Parser[A]): Parser[List[A]] =
-      p.map2(p.many)(_+:_) or succeed(List.empty)
+      p.map2(p.many)(_ +: _) or succeed(List.empty)
 
     // Recognize one or more a-s.
     def many1[A](p: Parser[A]): Parser[List[A]] =
-      p.map2(p.many)(_+:_)
+      p.map2(p.many)(_ +: _)
 
     // Apply f to the result of p, if successful.
-    def map[A, B](p: Parser[A])(f: A => B): Parser[B] = ???
+    def map[A, B](p: Parser[A])(f: A => B): Parser[B] =
+      p.flatMap(a => succeed(f(a)))
 
-    // Always succeed with the value a.
-    def succeed[A](a: A): Parser[A] = string("") map (_ => a)
+    def map2[A, B, C](p1: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] =
+      p1.flatMap(a => p2.map(b => f(a, b)))
+
+    def map2_v0[A, B, C](p1: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] =
+      (p1 ** p2) map (f.tupled)
+
+    def map2_v1[A, B, C](p1: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] =
+      for {
+        a <- p1
+        b <- p2
+      } yield f(a, b)
+
+    // Choose between p1 and p2. First attempt: p1. If p1 fails, choose p2.
+    def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A] = ???
+
+    implicit def regex(rx: Regex): Parser[String] = ???
 
     // Return a portion of the input string inspected by p if successful
     def slice[A](p: Parser[A]): Parser[String] = ???
 
+    // Recognizes and returns a single string
+    implicit def string(s: String): Parser[String] = ???
+
+    // Always succeed with the value a.
+    def succeed[A](a: A): Parser[A] = string("") map (_ => a)
+
     // Run p1 followed by p2, assuming p1 was successful
-    def product[A, B](p1: Parser[A], p2: => Parser[B]): Parser[(A, B)] = ???
+    def product[A, B](p1: Parser[A], p2: => Parser[B]): Parser[(A, B)] =
+      p1.flatMap(a => p2.map(b => (a, b)))
 
-    def map2[A, B, C](p: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] =
-      (p ** p2) map (f.tupled)
-
+    def product_v0[A, B](p1: Parser[A], p2: => Parser[B]): Parser[(A, B)] =
+      for {
+        a <- p1
+        b <- p2
+      } yield (a, b)
 
     def run[A](p: Parser[A])(input: String): Either[ParseError, A] = ???
 
+
     case class ParserOps[A](p: Parser[A]) {
-      def | [B>:A] (p2: Parser[B]): Parser[B] = self.or(p, p2)
-      def or [B>:A] (p2: => Parser[B]): Parser[B] = self.or(p, p2)
+      def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+
+      def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
+
       def many: Parser[List[A]] = self.many(p)
+
       def many1: Parser[List[A]] = self.many1(p)
+
       def map[B](f: A => B): Parser[B] = self.map(p)(f)
+
       def slice: Parser[String] = self.slice(p)
+
       def run(input: String): Either[ParseError, A] = self.run(p)(input)
-      def product [B] (p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
-      def map2[B, C] (p2: Parser[B])(f: (A, B) => C): Parser[C] = self.map2(p, p2)(f)
-      def flatMap[B] (f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
+
+      def product[B](p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
+
+      def map2[B, C](p2: Parser[B])(f: (A, B) => C): Parser[C] = self.map2(p, p2)(f)
+
+      def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
 
       // TODO copy-paste busted! alias?
-      def ** [B](p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
+      def **[B](p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
     }
 
 
@@ -90,25 +117,27 @@ object ch9_parse {
 
       def orLaws(in: Gen[String]): Prop =
         Prop.forAll(in)(s => (s | "random").run(s) == Right(s)) &&
-        Prop.forAll(in)(s => ("random" | s).run(s) == Right(s)) &&
-        Prop.forAll(in)(s => (succeed(s) | s) == succeed(s))
+          Prop.forAll(in)(s => ("random" | s).run(s) == Right(s)) &&
+          Prop.forAll(in)(s => (succeed(s) | s) == succeed(s))
 
       def succeedLaw[A](a: A)(in: Gen[String]): Prop =
         Prop.forAll(in)(s => succeed(a).run(s) == Right(a))
 
-//      def productLaws[A](in: Gen[String]): Prop =
-//        (p ** q) map f == (p map f) ** (q map f)
-//        p ** (q ** r) == (p ** q) ** r
+      //      def productLaws[A](in: Gen[String]): Prop =
+      //        (p ** q) map f == (p map f) ** (q map f)
+      //        p ** (q ** r) == (p ** q) ** r
 
       def basic: Boolean =
         run(listOfN(3, "ya" | "ba"))("yabba dabba doo") == Right("yabba dabba doo") &&
-        ("a" | "b").many.slice.run("abba") == Right("abba") && // TODO should work with chars
-        char('a').many.slice.map(_.size).run("abba") == Right(2) &&
-        char('c').many.slice.map(_.size).run("abba") == Right(0) &&  // Right!!!
-        (char('a').many.slice.map(_.size) ** char('b').many1.slice.map(_.size)).run("aabb") == Right(2) && // 0 or more 'a' followed by one or more 'b'
-        regex("[0..9]".r).flatMap(s => listOfN(s.toInt, "a")).run("2aa") == Right("2aa") // n followed by n 'a's
+          ("a" | "b").many.slice.run("abba") == Right("abba") && // TODO should work with chars
+          char('a').many.slice.map(_.size).run("abba") == Right(2) &&
+          char('c').many.slice.map(_.size).run("abba") == Right(0) && // Right!!!
+          (char('a').many.slice.map(_.size) ** char('b').many1.slice.map(_.size)).run("aabb") == Right(2) && // 0 or more 'a' followed by one or more 'b'
+          regex("[0..9]".r).flatMap(s => listOfN(s.toInt, "a")).run("2aa") == Right("2aa") // n followed by n 'a's
     }
+
   }
+
 
   def main(args: Array[String]): Unit = {
 
