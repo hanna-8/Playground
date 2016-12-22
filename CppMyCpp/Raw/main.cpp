1a1c9538e7da9d2ddbc4e4e2caafeb5ad3a31854
U-ENDAVA\ibarcan <ibarcan@EN617085.endava.net>
2016-12-22 10:57:20 +0200

    Cpp: Added performance tests for matrix representations.

--- CppMyCpp/Raw/main.cpp
+++ CppMyCpp/Raw/main.cpp
@@ -1,46 +1,14 @@
-#include <memory>
-#include <algorithm>
-
-#include <stdlib.h>
 #include <chrono>
-#include <stdio.h>
-
-
-
-class Vehicle {};
-class Car: public Vehicle {};
-
-class Metal{};
-class Iron: public Metal {};
-
-class VehicleFactory {
-    public: 
-        virtual Vehicle* create(Metal*) const { return new Vehicle(); }
-        virtual ~VehicleFactory() {}
-};
-
-class CarFactory: public VehicleFactory {
-    public:
-        virtual Car* create(Metal*) const override { return new Car(); }
-        virtual ~CarFactory() {}
-};
+#include <iostream>
 
 int main() {
-    
+
     auto start = std::chrono::system_clock::now();
 
-    std::shared_ptr<Vehicle> vehicle;
-    std::shared_ptr<Car> car;
 
-    vehicle = car;
-    //car = vehicle;
+    auto end = std::chrono::system_clock::now();
 
-
-      
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
     
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start);
-
-    printf("Time: %d ms\n", duration.count());
-
-    return 0;
+    std::cout << "t2 = " << duration.count() << std::endl;
 }
