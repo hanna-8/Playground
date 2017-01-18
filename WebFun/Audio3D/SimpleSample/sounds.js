9671c9952a670906b3b606e49d30c72faca53307
Ioana Barcan <ioana.barcan@endava.com>
2017-01-18 19:17:34 +0200

    Web: First javascript lines. Wo-hoo.

---
+++ WebFun/Audio3D/SimpleSample/sounds.js
@@ -0,0 +1,15 @@
+function left() {
+    document.getElementById("text").innerHTML = "left";
+}
+
+function right() {
+    document.getElementById("text").innerHTML = "right";
+}
+
+function up() {
+    document.getElementById("text").innerHTML = "up";
+}
+
+function down() {
+    document.getElementById("text").innerHTML = "down";
+}
