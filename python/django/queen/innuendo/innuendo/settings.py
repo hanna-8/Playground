4a1d7ded8e50419c6f6bf7f36e87a43d8a2ad7b6
i-bar <=>
2018-09-19 11:05:37 +0300

    Getting ready to get ready for deployment.

--- python/django/queen/innuendo/innuendo/settings.py
+++ python/django/queen/innuendo/innuendo/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'innuendo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'queen',
-        'USER': 'postgres',
-        'PASSWORD': 'root123',
-        'HOST': 'localhost',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
     }
 }
 
